2ff4cbdcf798a3e58bcc80fc265a3097e309b4f6
MAtakanicel <icelatakan@gmail.com>
2025-10-01 22:45:44 +0300

    icon ekleme

--- DailyNest/Views/AuthViews/LoginView.swift
+++ DailyNest/Views/AuthViews/LoginView.swift
@@ -1,7 +1,17 @@
 import SwiftUI
 struct LoginView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20){
+            Image(.icon)
+                .resizable()
+                .frame(width: 250, height: 250)
+
+            
+            
+            
+            
+        } //VStack
+        .background(AppColors.background)
     }
 }
 
