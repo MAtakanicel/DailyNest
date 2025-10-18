856f0cddd083e8cf4b2d5cc972d9cc721fcf68a8
MAtakanicel <icelatakan@gmail.com>
2025-10-18 23:36:13 +0300

    Modeller eklendi

--- DailyNest/Models/RoutineModel.swift
+++ DailyNest/Models/RoutineModel.swift
@@ -11,4 +11,15 @@ struct Routine: Schedulable {
     var routineDays: [WeekDay]
     var isCompletedToday: Bool
     var reminderTime: Date?
+    
+    init(id: UUID, description: String? = nil, isReminder: Bool, title: String, createdAt: Date, routineDays: [WeekDay], isCompletedToday: Bool, reminderTime: Date? = nil) {
+        self.id = id
+        self.description = description
+        self.isReminder = isReminder
+        self.title = title
+        self.createdAt = createdAt
+        self.routineDays = routineDays
+        self.isCompletedToday = isCompletedToday
+        self.reminderTime = reminderTime
+    }
 }
--- DailyNest/Models/TaskModel.swift
+++ DailyNest/Models/TaskModel.swift
@@ -13,4 +13,17 @@ struct Task: Schedulable {
     var completedAt: Date?
     var priority: ToDoPriority?
     var reminderDate: Date?
+    
+    init(id: UUID, description: String? = nil, isReminder: Bool, title: String, createdAt: Date, date: Date? = nil, isCompleted: Bool, completedAt: Date? = nil, priority: ToDoPriority? = nil, reminderDate: Date? = nil) {
+        self.id = id
+        self.description = description
+        self.isReminder = isReminder
+        self.title = title
+        self.createdAt = createdAt
+        self.date = date
+        self.isCompleted = isCompleted
+        self.completedAt = completedAt
+        self.priority = priority
+        self.reminderDate = reminderDate
+    }
     }
