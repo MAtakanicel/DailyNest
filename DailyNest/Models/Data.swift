8ad6e78e9fff9658c451db57cdce01e222548be0
MAtakanicel <icelatakan@gmail.com>
2025-10-18 23:57:06 +0300

    Mock Data taslak

--- DailyNest/Models/Data.swift
+++ DailyNest/Models/Data.swift
@@ -7,11 +7,22 @@ struct Data{
         Routine(id: UUID(), description: nil, isReminder: false, title: "Sabah yürüyüşü", createdAt: Date(), routineDays: [.saturday,.wednesday], isCompletedToday: false, reminderTime: nil),
         Routine(id: UUID(), description: "ACil", isReminder: false, title: "İlaç", createdAt: Date(), routineDays: WeekDay.allCases, isCompletedToday: false, reminderTime: nil),
         Routine(id: UUID(), description: "Deneme", isReminder: true, title: "Deneme", createdAt: Date(), routineDays: [.thursday,.sunday], isCompletedToday: true, reminderTime: nil),
+        Routine(id: UUID(), description: <#T##String?#>, isReminder: <#T##Bool#>, title: <#T##String#>, createdAt: <#T##Date#>, routineDays: <#T##[WeekDay]#>, isCompletedToday: <#T##Bool#>, reminderTime: <#T##Date?#>),
+        Routine(id: UUID(), description: <#T##String?#>, isReminder: <#T##Bool#>, title: <#T##String#>, createdAt: <#T##Date#>, routineDays: <#T##[WeekDay]#>, isCompletedToday: <#T##Bool#>, reminderTime: <#T##Date?#>),
+        Routine(id: UUID(), description: <#T##String?#>, isReminder: <#T##Bool#>, title: <#T##String#>, createdAt: <#T##Date#>, routineDays: <#T##[WeekDay]#>, isCompletedToday: <#T##Bool#>, reminderTime: <#T##Date?#>),
+        Routine(id: UUID(), description: <#T##String?#>, isReminder: <#T##Bool#>, title: <#T##String#>, createdAt: <#T##Date#>, routineDays: <#T##[WeekDay]#>, isCompletedToday: <#T##Bool#>, reminderTime: <#T##Date?#>),
     ]
     
     
     static let mockTasks : [Task] = [
         Task(id: UUID(), description: "Deneme2", isReminder: true, title: "Deno", createdAt: Date(), date: nil, isCompleted: false, completedAt: nil, priority: nil, reminderDate: nil),
+        Task(id: UUID(), description: "DenemeTo", isReminder: false, title: "DemeTo", createdAt: Date(), date: nil, isCompleted: true, completedAt: nil, priority: .high, reminderDate: nil),
+        Task(id: UUID(), description: <#T##String?#>, isReminder: <#T##Bool#>, title: <#T##String#>, createdAt: <#T##Date#>, date: <#T##Date?#>, isCompleted: <#T##Bool#>, completedAt: <#T##Date?#>, priority: <#T##ToDoPriority?#>, reminderDate: <#T##Date?#>),
+        Task(id: UUID(), description: <#T##String?#>, isReminder: <#T##Bool#>, title: <#T##String#>, createdAt: <#T##Date#>, date: <#T##Date?#>, isCompleted: <#T##Bool#>, completedAt: <#T##Date?#>, priority: <#T##ToDoPriority?#>, reminderDate: <#T##Date?#>),
+        Task(id: UUID(), description: <#T##String?#>, isReminder: <#T##Bool#>, title: <#T##String#>, createdAt: <#T##Date#>, date: <#T##Date?#>, isCompleted: <#T##Bool#>, completedAt: <#T##Date?#>, priority: <#T##ToDoPriority?#>, reminderDate: <#T##Date?#>),
+        Task(id: UUID(), description: <#T##String?#>, isReminder: <#T##Bool#>, title: <#T##String#>, createdAt: <#T##Date#>, date: <#T##Date?#>, isCompleted: <#T##Bool#>, completedAt: <#T##Date?#>, priority: <#T##ToDoPriority?#>, reminderDate: <#T##Date?#>),
+        Task(id: UUID(), description: <#T##String?#>, isReminder: <#T##Bool#>, title: <#T##String#>, createdAt: <#T##Date#>, date: <#T##Date?#>, isCompleted: <#T##Bool#>, completedAt: <#T##Date?#>, priority: <#T##ToDoPriority?#>, reminderDate: <#T##Date?#>),
+        Task(id: UUID(), description: <#T##String?#>, isReminder: <#T##Bool#>, title: <#T##String#>, createdAt: <#T##Date#>, date: <#T##Date?#>, isCompleted: <#T##Bool#>, completedAt: <#T##Date?#>, priority: <#T##ToDoPriority?#>, reminderDate: <#T##Date?#>),
     ]
     
 }
